2475717ff41231d239f75a4228a34fb8b9467c61
Nishant <nishants440@gmail.com>
2022-07-05 21:45:45 +0530

    Elec bill calc V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+//Import useState hook
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //create two input fields
+  const [prev, setprev] = useState(null);
+  const [current, setCurrent] = useState(null);
+  const [rate, setRate] = useState(null);
+  const [totalBill, setTotalBill] = useState(0);
+  const calculate = () => {
+    //substract prev from current
+    const result = current - prev;
+    setTotalBill(result * 7);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-container">
+        <h2>Electricity Bill Calculator</h2>
+      </div>
+      <div>
+        <input
+          type="number"
+          value={prev}
+          className="input"
+          placeholder="Previous Month Reading"
+          onChange={(e) => setprev(e.target.value)}
+        />
+        <input
+          type="number"
+          value={current}
+          className="input"
+          placeholder="Current Month Reading"
+          onChange={(e) => setCurrent(e.target.value)}
+        />
+      </div>
+      <input
+        type="number"
+        value={rate}
+        className="input"
+        placeholder="Rate of Charge"
+        onChange={(e) => setRate(e.target.value)}
+      />
+
+      <h3 className="total-bill">
+        Your Total Bill is Rs.{Math.round(totalBill)}
+      </h3>
+      <button className="button" onClick={calculate}>
+        Calculate
+      </button>
     </div>
   );
 }
